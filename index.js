b28097c2ecf963dd9d9c24fbfc61b5de0887aba4
3O1 <jamesbrandonedwards301@gmail.com>
2021-08-13 02:37:11 -0400

    setup password & email validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const auth = require("./routes/auth");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/auth", auth);
 
 app.get("/", (req, res) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,28 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.send("auth route working 🥳");
-});
+router.post(
+  "/signup",
+  [
+    check(email).isEmail(),
+    check(password).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const { password, email } = req.body;
+
+    console.log(password, email);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    res.send("auth route working 🥳");
+  }
+);
 
 module.exports = router;
