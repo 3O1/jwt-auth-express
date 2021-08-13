c0e0becc9b3635d723cc2963d46d627785d442e7
3O1 <jamesbrandonedwards301@gmail.com>
2021-08-13 01:46:28 -0400

    connected auth route

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const auth = require("./routes/auth");
 
 const app = express();
+
+app.use("/auth", auth);
+
+app.get("/", (req, res) => {
+  res.send("hello-world");
+});
 
 app.listen(5000, () => {
   console.log("server running on port 5000");
